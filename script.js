8c25121b603abf2dd737a5cba132109a22180da5
Siddharth <siddhusreeni@gmail.com>
2024-11-23 01:31:46 +0530

    video pages added

--- script.js
+++ script.js
@@ -38,8 +38,6 @@ function navAnimation() {
     });
   });
 }
-navAnimation()
-
 function page2Animation() {
   var rightElems = document.querySelectorAll(".right-elem");
 
@@ -66,4 +64,41 @@ function page2Animation() {
     });
   });
 }
-page2Animation()
+function page3VideoAnimation() {
+  var page3Center = document.querySelector(".page3-center");
+  var video = document.querySelector("#page3 video");
+
+  page3Center.addEventListener("click", () => {
+    gsap.to(video, {
+      transform: "scaleX(1) scaleY(1)",
+      opacity: 1,
+      borderRadius: 0,
+    });
+    video.play();
+  });
+  video.addEventListener("click", () => {
+    video.load();
+    gsap.to(video, {
+      transform: "scaleX(0.7) scaleY(0)",
+      opacity: 0,
+      borderRadius: "30px",
+    });
+  });
+}
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave", function () {
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
+
+// navAnimation()
+page2Animation();
+page3VideoAnimation();
